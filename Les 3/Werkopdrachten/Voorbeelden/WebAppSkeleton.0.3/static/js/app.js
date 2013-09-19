f102f10ade684b4c83d200bdaf8fde351d3ff773
Rizki Calame <rizki@rizkicalame.com>
2013-09-19 11:28:10 +0200

    Skeleton added

---
+++ Les 3/Werkopdrachten/Voorbeelden/WebAppSkeleton.0.3/static/js/app.js
@@ -0,0 +1,98 @@
+var APP = APP || {};
+
+(function () {
+	// Data objecten
+	APP.page1 = {
+		title:'Pagina 1',
+		description:'Pagina 1 is de eerste pagina',
+		schedule: [
+    { date: "Monday, 9:00am", team1: "Chasing", team1Score: "13", team2: "Amsterdam Money Gang", team2Score: "9"},
+    { date: "Monday, 9:00am", team1: "Boomsquad", team1Score: "15", team2: "Beast Amsterdam", team2Score: "11"},
+    { date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "14", team2: "Amsterdam Money Gang", team2Score: "12"},
+    { date: "Monday, 10:00am", team1: "Chasing", team1Score: "5", team2: "Burning Snow", team2Score: "15"},
+    { date: "Monday, 11:00am", team1: "Boomsquad", team1Score: "11", team2: "Amsterdam Money Gang", team2Score: "15"},    
+    { date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "15", team2: "Beast Amsterdam", team2Score: "6"},
+    { date: "Monday, 12:00pm", team1: "Chasing", team1Score: "8", team2: "Beast Amsterdam", team2Score: "15"},
+    { date: "Monday, 12:00pm", team1: "Boomsquad", team1Score: "15", team2: "Burning Snow", team2Score: "8"},
+    { date: "Monday, 1:00pm", team1: "Chasing", team1Score: "15", team2: "Boomsquad", team2Score: "14"},
+    { date: "Monday, 1:00pm", team1: "Burning Snow", team1Score: "15", team2: "Amsterdam Money Gang", team2Score: "11"}
+			
+		]
+	};
+
+	APP.page2 = {
+		title:'Pagina 2',
+		description:'Pagina 2 is de tweede pagina'
+	};
+
+	APP.page3 = {
+		title:'Pagina 3',
+		description:'Pagina 3 is de derde pagina'
+	};
+	
+	// Controller Init
+	APP.controller = {
+		init: function () {
+			// Initialize router
+			APP.router.init();
+		}
+	};
+
+	// Router
+	APP.router = {
+		init: function () {
+	  		routie({
+			    '/page1': function() {
+			    	APP.page.render('page1');
+				},
+			    '/page2': function() {
+			    	APP.page.render('page2');
+			    },
+
+			    '/page3': function() {
+			    	APP.page.render('page3');
+			    },
+			    '*': function() {
+			    	APP.page.render('page1');
+			    }
+			});
+		},
+
+		change: function () {
+            var route = window.location.hash.slice(2),
+                sections = qwery('section'),
+                section = qwery('[data-route=' + route + ']')[0];
+
+            // Show active section, hide all other
+            if (section) {
+            	for (var i=0; i < sections.length; i++){
+            		sections[i].classList.remove('active');
+            	}
+            	section.classList.add('active');
+            }
+
+            // Default route
+            if (!route) {
+            	sections[0].classList.add('active');
+            }
+
+		}
+	};
+
+	// Pages
+	APP.page = {
+		render: function (route) {
+			// http://javascriptweblog.wordpress.com/2010/04/19/how-evil-is-eval/
+			var data = eval('APP.'+route);
+
+			Transparency.render(qwery('[data-route='+route+']')[0], data);
+			APP.router.change();
+		}
+	}
+	// DOM ready
+	domready(function () {
+		// Kickstart application
+		APP.controller.init();
+	});
+	
+})();
