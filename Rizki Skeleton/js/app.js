4e3eaa0731c32b647b58498dcbd742f22649839b
Rizki Calame <rizki@rizkicalame.com>
2013-10-31 20:06:28 +0100

    Moar adjustments + pre game_scores

--- Rizki Skeleton/js/app.js
+++ Rizki Skeleton/js/app.js
@@ -4,23 +4,24 @@ var FRISBEEAPP = FRISBEEAPP || {};
 
 	'use strict';
 	
-	// Schedule data object
+	//Schedule data object
 	FRISBEEAPP.schedule = {
 		schedule: [
 		]
 	};
-
+	//Game data object
 	FRISBEEAPP.game = {
 		game: [
 		]
 	};
 
-	// //Ranking data object
+	//Ranking data object
 	FRISBEEAPP.ranking = {
 		rank: [
 		]
 	};
 
+	//Some utilities
 	FRISBEEAPP.utilities = {
 		spinner: {
 			spinnerObject: document.getElementById("spinner"), 
@@ -30,6 +31,12 @@ var FRISBEEAPP = FRISBEEAPP || {};
 			hide: function () {
 				this.spinnerObject.className ="stopspin";
 			}
+		},
+
+		error: {
+			alert: function (reason, message) {
+				alert(reason + " " + message);
+			}
 		}
 	};
 	
@@ -38,6 +45,7 @@ var FRISBEEAPP = FRISBEEAPP || {};
 		init: function () {
 			// Initialize router
 			FRISBEEAPP.router.init();
+			FRISBEEAPP.gestures.init();
 		}
 	};
 
@@ -57,7 +65,6 @@ var FRISBEEAPP = FRISBEEAPP || {};
 			    },
 			    '*': function() {
 			    	FRISBEEAPP.ajax.getObjectsForSchedule();
-			    	FRISBEEAPP.page.render("schedule");
 			    }
 			});
 		},
@@ -70,7 +77,7 @@ var FRISBEEAPP = FRISBEEAPP || {};
         		sections[i].classList.remove('active');
         	}
 			
-			//Check if slash is there or not
+			//Check if slash is there or not for gameID
 			if(route.search("/") != -1) {
 				route = route.substring(0, route.search("/"));
 			}
@@ -79,7 +86,7 @@ var FRISBEEAPP = FRISBEEAPP || {};
 
         	sectionToChange.classList.add('active');
 
-        	// default route
+        	// If no route is found, default is schedule
         	if (!route) {
         		sections[0].classList.add('active');
         	}
@@ -92,8 +99,11 @@ var FRISBEEAPP = FRISBEEAPP || {};
 		render: function (route) {
 			var data = FRISBEEAPP[route];
 
+			//Add data to some attributes
 			var directives = {
+				// Array of data
 				schedule: {
+					//ID of the to be changed
 					link: {
 						href: function(params) {
 							return "#/game/" + this.gameID;
@@ -121,21 +131,26 @@ var FRISBEEAPP = FRISBEEAPP || {};
 	}
 
 	FRISBEEAPP.ajax = {
+		//Grabs the objects for the ranking page
 		getObjectsForRanking: function () {
+			//Feed URL for ranking
 			var feed = "https://api.leaguevine.com/v1/pools/?tournament_id=19389&order_by=%5Bname%5D";
 
+			// Fixes visual bug where DOM shows some objects
+			document.querySelector('article > section:nth-of-type(3) > section').classList.remove("show");
+			document.querySelector('article > section:nth-of-type(3) > section').classList.add("hide");
+
 			FRISBEEAPP.utilities.spinner.show();
-			// data is duidelijker als text
+
 			promise.get(feed).then(function(error, data, xhr){
 				if (error) {
-       				alert('Error ' + xhr.status);
+       				FRISBEEAPP.utilities.error.alert("Request timed out. Error code: ");
         			return;
     			}	
 				
 				data = JSON.parse(data)
-				// Zo hoef je geen nieuwe var aan te maken en blijft de var duidelijk =)
 
-				// For elke pool in de api
+				// For every pool in the api get poolname
 				for (var i = 0; i < data.objects.length; i++) {
 					var poolName = data.objects[i].name;
 
@@ -145,7 +160,7 @@ var FRISBEEAPP = FRISBEEAPP || {};
 
 					FRISBEEAPP.ranking.rank[i].teams = [];
 
-					// For elke team binnen een pool
+					// For every team in a pool grab data
 					for (var c = 0; c < data.objects[i].standings.length; c++) {
 						FRISBEEAPP.ranking.rank[i].teams[c] = {
 						 	team: data.objects[i].standings[c].team.name,
@@ -157,6 +172,9 @@ var FRISBEEAPP = FRISBEEAPP || {};
 						};
 					}	
 				}
+				//Shows elements after array has been filled
+				document.querySelector('article > section:nth-of-type(3) > section').classList.remove("hide");
+				document.querySelector('article > section:nth-of-type(3) > section').classList.add("show");
 				FRISBEEAPP.utilities.spinner.hide();
 				FRISBEEAPP.page.render('ranking');
 			});
@@ -168,10 +186,9 @@ var FRISBEEAPP = FRISBEEAPP || {};
 			document.querySelector('article > section:nth-of-type(1) > section').classList.add("hide");
 			FRISBEEAPP.utilities.spinner.show();
 			
-			// Zie functie hierboven voor zelfde verandering (data)
 			promise.get(feed).then(function(error, data, xhr) {
 				if (error) {
-       				alert('Error ' + xhr.status);
+       				FRISBEEAPP.utilities.error.alert("Request timed out. Error: ");
         			return;
     			}	
 
@@ -222,6 +239,8 @@ var FRISBEEAPP = FRISBEEAPP || {};
     			data = JSON.parse(data);	
 
     			FRISBEEAPP.game.game = {
+    				team1Name: data.team_1.name,
+    				team2Name: data.team_2.name,
     				team1Score: data.team_1_score,
     				team2Score: data.team_2_score
     			}
@@ -229,9 +248,68 @@ var FRISBEEAPP = FRISBEEAPP || {};
     			FRISBEEAPP.page.render('game');
     			FRISBEEAPP.utilities.spinner.hide();
     		});
+		},
+
+		saveNewScore: function () {
+			//Locate gameID from URL
+			var gameID = window.location.hash.slice(2);
+			if(gameID.search("/") != -1) {
+				//FIX THE SUBSTRRRR
+				gameID = gameID.substr(gameID.search("/") + 1 , 6);
+			}	
+
+			//Grab the value out of the input types
+			var team1Score = document.getElementById('team1Score').value;
+			var team2Score = document.getElementById('team2Score').value;
+
+			//Fire updatenewScore function
+			FRISBEEAPP.ajax.updateNewScore(gameID, team1Score, team2Score, false);
+		},
+
+		updateNewScore: function (gameID, team1Score, team2Score, isFinal) {
+			FRISBEEAPP.utilities.spinner.show();
+
+			//Make new request object
+	        var request = new XMLHttpRequest();
+
+	        //The data to adjust
+	        var dataToSend = {
+	                "game_id": gameID,
+	                "team_1_score": team1Score,
+	                "team_2_score": team2Score,
+	                "final": isFinal
+	        };
+
+	        //Set method, url and if async
+	        request.open("POST","https://api.leaguevine.com/v1/game_scores/",true);
+
+	        //Set request subject and content
+	        request.setRequestHeader("Content-Type","application/json");
+	        request.setRequestHeader("Accept","application/json");
+	        request.setRequestHeader("Authorization","bearer 74884efad3");
+
+	        //Stringify data
+	        request.send(JSON.stringify(dataToSend));
+
+	        //If reeadystate changes do stuff
+	        request.onreadystatechange = function() {
+                  if(request.readyState == 4) {
+                  	FRISBEEAPP.utilities.spinner.hide();
+                  	FRISBEEAPP.utilities.error.alert("Posted new score.", "Check the schedule page for the update!");
+                  	console.log("Post success!");
+                  } document.getElementById("gesture")
+	         }
 		}
 	}
 
+	FRISBEEAPP.gestures = {
+		init: function () {
+			$$(".gesture").swipe(function () {
+				Fader.fadeOutWithId("changescore", 1);
+				console.log('swipe');
+			});
+		}
+	}
 
 	// If DOM == ready, fire function:
 	domready(function () {
